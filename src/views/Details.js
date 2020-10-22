26d8e21e6d0cd3ab9515edb50aef9334b977d158
Lucas <lucasdcifuentes@gmail.com>
2020-10-22 10:03:14 -0500

    create details

--- src/views/Details.js
+++ src/views/Details.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import styled from "styled-components";
+import { useParams, Link } from "react-router-dom"
+
+const Button = styled.div`
+  border: none;
+  background-color: transparent;
+  margin: 2rem;
+  padding: 0.5rem 1rem;
+  box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.3);
+  width: 4rem;
+  a{
+    color: #2196f3;
+    text-decoration: none;
+  }
+`
 
 const Details = () => {
+
+  const { country } = useParams();
+
   return (
     <>
+      <Button>
+      <Link to="/" > <i className="fas fa-arrow-left"></i> Back</Link>
+      </Button>
       <h1>Details</h1>
+      <p> {country} </p>
     </>
   )
 }
